335fa75dcfdb1b4f607be2ea99a0049436464730
BryantWan <bryantwan2003@gmail.com>
2026-05-18 14:38:14 -0400

    reuploaded

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="heading">
+        
+      </section>
+
+      <section id="profile">
+        
+      </section>
+
+      <section id="blog">
+        
+      </section>
+
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
